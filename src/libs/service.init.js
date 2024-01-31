9f6557582e1c293d3764042ef340f78cde82f891
permaficus <abukhalif2019@gmail.com>
2024-01-31 20:57:00 +0700

    Fixed: caching method
    Updated: how we presenting cache result to client
    Fixed: cache data doubled in result

--- src/libs/service.init.js
+++ src/libs/service.init.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { router as v1 } from '../v1/router/router.js';
 import { SERVICE_LOCAL_PORT, NODE_ENV } from '../constant/config.js';
+import cors from 'cors';
 
 const httpServer = new express();
 
 const serverInit = () => {
     httpServer.use(express.urlencoded({ extended: true }));
     httpServer.use(express.json());
+    httpServer.use(cors())
     httpServer.use('/v1', v1)
 }
 
--- src/v1/controller/index.js
+++ src/v1/controller/index.js
@@ -6,6 +6,7 @@ const regenciesData = 'src/data/regencies.json';
 
 export const FetchingData = async (req, res) => {
     const { model, pid } = req.params
+    let rawData = {}
     if (!model && !pid) {
         res.status(400).json({
             status: 'ERR_BAD_REQUEST',
@@ -22,7 +23,13 @@ export const FetchingData = async (req, res) => {
                 : { data: await readFile(regenciesData, 'utf-8'), fileName: 'city' }
         }
         if (read.data) {
-            model == 'province' ? await redisCache('set', read.fileName, read.data) : await redisCache('set', `${read.fileName}:${pid}`, read.data)
+            if (model === 'city') {
+                rawData = structuredClone(JSON.parse(read.data).filter(e => e.pid === pid))
+
+            }
+            model == 'province' ? 
+                await redisCache('set', read.fileName, read.data) 
+                : await redisCache('set', `${read.fileName}:${pid}`, JSON.stringify(rawData))
         }
         res.status(200).json({
             status: 'OK',
@@ -30,6 +37,7 @@ export const FetchingData = async (req, res) => {
             result: model == 'province' ? JSON.parse(read.data) : JSON.parse(read.data).filter(e => e.pid === pid)
         })
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             status: 'INTERNAL_SERVER_ERROR',
             code: 500,
