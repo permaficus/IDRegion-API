93ef2786a502bfc62742c4d271cb2d943af77cf1
permaficus <abukhalif2019@gmail.com>
2024-05-05 10:51:29 +0700

    chore(cors): add allowedOrigin variable

--- src/constant/config.js
+++ src/constant/config.js
@@ -6,3 +6,9 @@ export const CACHING_METHOD = process.env.CACHING_METHOD || 'none'
 export const NODE_ENV = process.env.NODE_ENV
 export const COMPOSE_PROJECT_NAME = process.env.COMPOSE_PROJECT_NAME
 export const REDIS_URL = process.env.REDIS_URL
+
+/**
+ * A list for CORS policy
+ * Leave empty array to allow from all(*)
+ */
+export const allowedOrigin = ['http://localhost:8080']
